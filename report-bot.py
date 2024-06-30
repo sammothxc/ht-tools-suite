5c19051b9d63d88c180b26912f88a458c9f420e4
sammothxc <sdwarrior8@gmail.com>
2024-06-30 06:28:36 -0600

    usr.txt addition

--- report-bot.py
+++ report-bot.py
@@ -12,8 +12,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 def getOptions(args=sys.argv[1:]):
 
     parser = argparse.ArgumentParser(description="This bot helps users to mass report accounts with clickbaits or objectionable material.")
-    parser.add_argument("-u", "--username", type = str, default = "", help = "Username to report.")
-    parser.add_argument("-f", "--file", type = str, default = "acc.txt", help = "Accounts list ( Defaults to acc.txt in program directory ).")
+    parser.add_argument("-u", "--usernames", type = str, default = "usr.txt", help = "Usernames to report ( Defaults to usr.txt in program directory ).")
+    parser.add_argument("-a", "--accounts", type = str, default = "acc.txt", help = "Accounts list ( Defaults to acc.txt in program directory ).")
 
     options = parser.parse_args(args)
 
@@ -21,87 +21,89 @@ def getOptions(args=sys.argv[1:]):
 
 args = getOptions()
 
-username = args.username
-acc_file = args.file
+usr_file = args.usernames
+acc_file = args.accounts
 
-if username == "" :
-	username = input("Username: ")
+u = open(usr_file, "r").readlines()
+u_file = [s.rstrip()for s in u]
+u_file.reverse()
 
 a = open(acc_file, "r").readlines()
-file = [s.rstrip()for s in a]
-file.reverse()
+a_file = [s.rstrip()for s in a]
+a_file.reverse()
 
-user = []
-passw = []
-for lines in file:
-    file = lines.split(":")
+for username in u_file:
+    user = []
+    passw = []
+    for lines in a_file:
+        a_file = lines.split(":")
 
-    un = file[0]
-    pw = file[1]
-    user.append(un)
-    passw.append(pw)
+        un = a_file[0]
+        pw = a_file[1]
+        user.append(un)
+        passw.append(pw)
 
-for line in range(len(file)+1):
-    web = webdriver.Chrome()
+    for line in range(len(a_file)+1):
+        web = webdriver.Chrome()
 
-    web.get("https://www.instagram.com/accounts/login/")
-    assert "Instagram" in web.title
+        web.get("https://www.instagram.com/accounts/login/")
+        assert "Instagram" in web.title
 
-    time.sleep(1.5)
+        time.sleep(1.5)
 
-    elem_user = web.find_element(By.NAME, "username")
-    elem_user.send_keys(user[line])
-    time.sleep(0.7)
-    elem_pass = web.find_element(By.NAME, "password")
-    time.sleep(0.7)
-    elem_pass.send_keys(passw[line])
-    time.sleep(0.7)
-    elem_pass.send_keys(Keys.ENTER)
+        elem_user = web.find_element(By.NAME, "username")
+        elem_user.send_keys(user[line])
+        time.sleep(0.7)
+        elem_pass = web.find_element(By.NAME, "password")
+        time.sleep(0.7)
+        elem_pass.send_keys(passw[line])
+        time.sleep(0.7)
+        elem_pass.send_keys(Keys.ENTER)
 
-    time.sleep(2.5)
+        time.sleep(2.5)
 
-    web.get("https://www.instagram.com/%s/" % username) # TO-DO: use links directly
+        web.get("https://www.instagram.com/%s/" % username) # TO-DO: use links directly
 
-    time.sleep(2.5)
+        time.sleep(2.5)
 
-    options = web.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/header/section[2]/div/div/div[3]/div')
+        options = web.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/header/section[2]/div/div/div[3]/div')
+        
+        ActionChains(web)\
+            .click(options)\
+            .perform()
+        
+        time.sleep(2)
+
+        report = web.find_element(By.XPATH, '/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div/button[3]')
+        
+        ActionChains(web)\
+            .click(report)\
+            .perform()
+
+        time.sleep(2)
+
+        report2 = web.find_element(By.XPATH, '/html/body/div[7]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div/div[3]/button[2]/div')
+        
+        ActionChains(web)\
+            .click(report2)\
+            .perform()
+
+        time.sleep(200)
+
+        report3 = web.find_element(By.XPATH, '/html/body/div[7]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div/div[3]/button[2]/div')
+        
+        ActionChains(web)\
+            .click(report3)\
+            .perform()
+
+        web.click(text='Log Out')
+
+        time.sleep(0.5)
+
+        pyautogui.keyDown('ctrl')
+        time.sleep(0.25)
+        pyautogui.keyDown('w')
+        time.sleep(0.5)
+        pyautogui.keyUp('ctrl')
+        pyautogui.keyUp('https://replit.com/@Cw')
     
-    ActionChains(web)\
-        .click(options)\
-        .perform()
-    
-    time.sleep(2)
-
-    report = web.find_element(By.XPATH, '/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div/button[3]')
-    
-    ActionChains(web)\
-        .click(report)\
-        .perform()
-
-    time.sleep(2)
-
-    report2 = web.find_element(By.XPATH, '/html/body/div[7]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div/div[3]/button[2]/div')
-    
-    ActionChains(web)\
-        .click(report2)\
-        .perform()
-
-    time.sleep(200)
-
-    report3 = web.find_element(By.XPATH, '/html/body/div[7]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div/div[3]/button[2]/div')
-    
-    ActionChains(web)\
-        .click(report3)\
-        .perform()
-
-    web.click(text='Log Out')
-
-    time.sleep(0.5)
-
-    pyautogui.keyDown('ctrl')
-    time.sleep(0.25)
-    pyautogui.keyDown('w')
-    time.sleep(0.5)
-    pyautogui.keyUp('ctrl')
-    pyautogui.keyUp('https://replit.com/@Cw')
- 
