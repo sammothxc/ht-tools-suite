8b27825d100398b6bd8c9b29ab69d2a20dc80147
sammothxc <sdwarrior8@gmail.com>
2024-07-01 04:17:34 -0600

    update

--- report-posts.py
+++ report-posts.py
@@ -19,7 +19,7 @@ def getOptions(args=sys.argv[1:]):
 
     return options
 
-def reportPost():
+def reportPost(web):
     optionDots = web.find_element(By.XPATH, '/html/body/div[8]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[1]/div/div/div/div')
     clickElement(optionDots)
     reportOption = web.find_element(By.XPATH, '/html/body/div[8]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div/button[1]')
@@ -34,7 +34,7 @@ def reportPost():
     clickElement(close)
     return None
 
-def nextPost():
+def nextPost(web):
     try:
         nextButton = web.find_element(By.XPATH, '/html/body/div[7]/div[1]/div/div[3]/div/div/div/div/div[1]/div/div/div[2]/button')
     except:
@@ -42,87 +42,70 @@ def nextPost():
     clickElement(nextButton)
     return None
 
-def clickElement(elem):
+def clickElement(elem, web):
     ActionChains(web).click(elem).perform()
     return None
 
-args = getOptions()
+def main():
+    args = getOptions()
 
-usr_file = args.usernames
-acc_file = args.accounts
+    usr_file = args.usernames
+    acc_file = args.accounts
 
-try:
-    u = open(usr_file, "r").readlines()
-    u_file = [s.rstrip()for s in u]
-except FileNotFoundError:
-    print("ERROR: no usr.txt file found")
-    exit(2)
+    try:
+        u = open(usr_file, "r").readlines()
+        u_file = [s.rstrip()for s in u]
+    except FileNotFoundError:
+        print("ERROR: no usr.txt file found")
+        exit(2)
 
-try:
-    a = open(acc_file, "r").readlines()
-    a_file = [s.rstrip()for s in a]
-    a_file.reverse()
-except FileNotFoundError:
-    print("ERROR: no acc.txt file found")
-    exit(2)
+    try:
+        a = open(acc_file, "r").readlines()
+        a_file = [s.rstrip()for s in a]
+        a_file.reverse()
+    except FileNotFoundError:
+        print("ERROR: no acc.txt file found")
+        exit(2)
 
-user = []
-passw = []
-
-web = webdriver.Firefox()
-web.implicitly_wait(10)
-
-for lines in a_file:
-    a_file = lines.split(":")
-
+    with open("pass.txt", "r") as pass_file:
+        a_file = pass_file.read().split(":")
     try:
         un = a_file[0]
         pw = a_file[1]
     except IndexError:
         print("ERROR: Wrong syntax in acc.txt")
-    user.append(un)
-    passw.append(pw)
+        exit(2)
 
-for line in range(len(a_file)+1):
+    web = webdriver.Firefox()
+    web.implicitly_wait(10)
+
     try:
         web.get("https://www.instagram.com/accounts/login/")
         assert "Instagram" in web.title
     except:
         print("ERROR: Failed to open the web browser.")
-        break
+        exit(2)
     
     elem_user = web.find_element(By.NAME, "username")
-    elem_user.send_keys(user[line])
-    time.sleep(0.7)
+    elem_user.send_keys(un)
     elem_pass = web.find_element(By.NAME, "password")
-    time.sleep(0.7)
-    elem_pass.send_keys(passw[line])
-    time.sleep(0.7)
+    elem_pass.send_keys(pw)
+    time.sleep(0.3)
     elem_pass.send_keys(Keys.ENTER)
-
     time.sleep(4)
     
     for username in u_file:
-        
-        web.implicitly_wait(10)
-
         web.get(username)
+        firstPost = web.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/div[2]/div/div[1]/div[1]')
+        clickElement(firstPost)
+        while True:
+            print("Report post? (y) / n")
+            report = input()
+            if report == "n":
+                nextPost(web)
+            else:
+                reportPost(web)
+                nextPost(web)            
 
-    row = 1
-    col = 1
-    while True:
-        print("Report post? (y) / n")
-        report = input()
-        if report == "n":
-            nextPost()
-        else:
-            reportPost()
-            nextPost()            
-
-
-        pyautogui.keyDown('ctrl')
-        time.sleep(0.25)
-        pyautogui.keyDown('w')
-        time.sleep(0.5)
-        pyautogui.keyUp('ctrl')
-        pyautogui.keyUp('https://replit.com/@Cw')
+if '__main__':
+    main()
